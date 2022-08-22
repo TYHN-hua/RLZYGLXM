220c8070fb38bbcea63fc2e1dc327993816eade2
Your Name <you@example.com>
2022-08-22 23:11:10 +0800

    组织架构页面渲染完成

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,29 +1,57 @@
 <template>
-  <div>
-    departments
+  <div class="departments-container">
+    <el-card class="tree-card">
+      <treeTools :tree-node="company" :is-root="false" />
+    </el-card>
+    <!--放置一个属性   这里的props和我们之前学习的父传子 的props没关系-->
+    <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
+      <treeTools slot-scope="{data}" :tree-node="data" />
+    </el-tree>
   </div>
 </template>
 
 <script>
+import treeTools from './components/tree-tools.vue'
+import { getDepartments } from '@/api/departments'
+import { tranListToTreeData } from '@/utils/index'
 export default {
   name: 'Hrsaas1Index',
-
+  components: {
+    treeTools
+  },
   data() {
     return {
-
+      defaultProps: {
+        label: 'name'
+      },
+      departs: [],
+      company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' }
     }
   },
 
   mounted() {
-
+    this.getDepartments()
   },
 
   methods: {
-
+    async getDepartments() {
+      const { depts, companyName, companyManage } = await getDepartments()
+      // console.log(res)
+      this.departs = tranListToTreeData(depts, '')
+      this.company = { name: companyName, manager: companyManage }
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+  .departments-container {
+    width: 900px;
+    margin: 20px auto;
+    .el-tree {
+      .el-tree-node__content {
+        padding-right: 20px;
+      }
+    }
+  }
 </style>
