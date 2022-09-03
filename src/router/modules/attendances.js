09b5f5d0c47f46441729b975010f462d5fab5edc
Your Name <you@example.com>
2022-09-03 09:45:19 +0800

    其他页面完成

--- src/router/modules/attendances.js
+++ src/router/modules/attendances.js
@@ -1,20 +1,38 @@
-// 导出员工的路由规则
+
 import Layout from '@/layout'
-export default {
-  // 路由规则
-  path: '/attendances', // 路由地址
+
+const attendRouter = {
+  path: '/attendances',
   component: Layout,
+  // name: 'attendances',
   meta: { id: 'attendances' },
-  children: [{
-    name: 'attendances', // 给模块的一级路由加一个name属性
-    // 二级路由path什么都不用写的时候 此时它表示二级路由的默认路由
-    path: '', // 这里不用写，不写的时候表示employees 不但有layout员工主页
-    // 动态按需加载，通过箭头函数的形式引入
-    component: () => import('@/views/attendances'),
-    // 路由的源信息 其实就是一个储存数据的地方，可以放任何内容
-    meta: {
-      title: '考勤', // 这里用title的原因是因为左侧导航读取了这里的title属性
-      icon: 'skill'
+  children: [
+    {
+      path: '',
+      component: () => import('@/views/attendances'),
+      name: 'attendances',
+      meta: {
+        title: '考勤',
+        icon: 'excel' }
+    },
+    {
+      path: 'archiving',
+      component: () => import('@/views/attendances/historical'),
+      name: 'archiving',
+      hidden: true,
+      meta: {
+        title: '归档'
+      }
+    },
+    {
+      path: 'report/:month',
+      component: () => import('@/views/attendances/report'),
+      name: 'reports',
+      hidden: true,
+      meta: {
+        title: '报表'
+      }
     }
-  }]
+  ]
 }
+export default attendRouter
